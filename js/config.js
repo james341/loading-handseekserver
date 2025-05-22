2b6bd79f7ee51629ee17d22cd3853d2c7d14015d
james341 <taine049@gmail.com>
2025-05-23 00:24:36 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -146,6 +146,7 @@ var l_musicPlaylist = [
 	{ogg: "sneighbourhood.ogg", name: "Installation Music"},
 	{ogg: "unseenhorrors.ogg", name: "Installation Music"},
 	{ogg: "fnafsecuritybreachmusic.ogg", name: "Installation Music"},
+	{ogg: "behindtheslaughteremix.ogg", name: "Installation Music"},
 	{ogg: "snapsonhideawaykbps.ogg", name: "Installation Music"},
 	{ogg: "fnafsbdayecareshit.ogg", name: "Installation Music"},
 	{ogg: "snapsonhideawaykbps.ogg", name: "Installation Music"},
